1492145157b2f50126edc07f37a89e9e7e09bd6e
Martin Chakarov <martin.d.chakarov@gmail.com>
2022-04-10 16:22:22 +0300

    Create app.js and basic views

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const consolidate = require('consolidate');
+const cons = require('consolidate');
 const dust = require('dustjs-helpers');
 const pg = require('pg');
 const app = express();
+
+
+// DB Connection String
+const connect = "postgres://demo_user:123456@localhost/recipes";
+
+// Assign Dust Engine to .dust files
+app.engine('dust', cons.dust);
+
+// Set Default Extension .dust
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
+// Server
+app.listen(3000, function() {
+    console.log('Server Started on Port 3000')
+})
